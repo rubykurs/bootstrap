84c5ab2cf2d5cbf562e3397e4c11c23e3728f004
Ole Morten Amundsen <ole.morten.amundsen@gmail.com>
2012-01-29 19:09:50 +0100

    devise admin

--- db/migrate/20120129135315_devise_create_admins.rb
+++ db/migrate/20120129135315_devise_create_admins.rb
@@ -6,8 +6,8 @@ class DeviseCreateAdmins < ActiveRecord::Migration
       t.string :encrypted_password, :null => false, :default => ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      #t.string   :reset_password_token
+      #t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
@@ -41,7 +41,7 @@ class DeviseCreateAdmins < ActiveRecord::Migration
     end
 
     add_index :admins, :email,                :unique => true
-    add_index :admins, :reset_password_token, :unique => true
+    #add_index :admins, :reset_password_token, :unique => true
     # add_index :admins, :confirmation_token,   :unique => true
     # add_index :admins, :unlock_token,         :unique => true
     # add_index :admins, :authentication_token, :unique => true
